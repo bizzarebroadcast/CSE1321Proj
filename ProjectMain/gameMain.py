604cdae5f933eb5389c1b8aeb3cdef20a97e0519
gavin <gavin.c.hawk@gmail.com>
2024-11-07 11:27:10 -0500

    Commit to fix code redundancies

--- ProjectMain/gameMain.py
+++ ProjectMain/gameMain.py
@@ -1,7 +1,7 @@
 import pygame
 import sys
 from gameChar import gameChar
-
+from gamePlatform import Platform
 pygame.init()
 
 screen = pygame.display.set_mode((1400, 800))
@@ -12,24 +12,14 @@ black = (0, 0, 0)
 
 char = gameChar(800, 750, screen)
 
-class platform:
-    def __init__(self, x, y, width, height, screen):
-        self.x = x
-        self.y = y
-        self.width = width
-        self.height = height
-        self.screen = screen
 
-    def draw(self):
-        pygame.draw.rect(self.screen, (0, 255, 0), (self.x, self.y, self.width, self.height))
-
-def platforms(screen):
+def platforms(self):
     return [
-        platform(200, 650, 200, 20, screen),
-        platform(500, 550, 200, 20, screen),
-        platform(800, 450, 200, 20, screen),
-        platform(1100, 350, 200, 20, screen),
-        platform(300, 250, 200, 20, screen)
+        Platform(200, 650, 200, 20, screen),
+        Platform(500, 550, 200, 20, screen),
+        Platform(800, 450, 200, 20, screen),
+        Platform(1100, 350, 200, 20, screen),
+        Platform(300, 250, 200, 20, screen)
     ]
 
 platform_list = platforms(screen)
